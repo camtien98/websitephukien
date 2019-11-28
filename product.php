e4b58dd64e1edf7c2905cf79ef33e4266911736a
camtien98 <57025344+camtien98@users.noreply.github.com>
2019-11-28 17:07:45 +0700

    sua xong tìm kiếm, thêm sản phẩm vào giỏ hàng

--- product.php
+++ product.php
@@ -40,46 +40,58 @@ function VndDot($strNum)
 </head>
 
 <body>
-    <header class="nav-top head">
-        <div class="container">
-            <div class="row">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="#">
-                        <!-- <img src="img/logo.png" class="logo" alt=""> -->
-                        Phụ Kiện Điện Tử TTShop
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse nav-right" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto ">
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.html">Trang Chủ
-                                    <span class="sr-only">(current)</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="about-us.php">Giới Thiệu</a>
-                            </li>
+    <div class="body">
+        <!-- body -->
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="product.php">Sản Phẩm</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="contact.php">Liên Hệ</a>
-                            </li>
-                           <li class="nav-item">
-                           <a href="session.php"><i class="fa fa-cart-plus" aria-hidden="true" ></i> Giỏ Hàng</a>
-                           </li>
-                      </div>
-                        </ul>
+        <header class="nav-top head">
+            <div class="container">
+                <div class="row">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="#">
+                            <!-- <img src="img/logo.png" class="logo" alt=""> -->
+                            Tiên Tiên
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse nav-right" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto ">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.html">Trang Chủ
+                                        <span class="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about-us.php">Giới Thiệu</a>
+                                </li>
+    
+                                <li class="nav-item">
+                                    <a class="nav-link" href="product.php">Sản Phẩm</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.php">Liên Hệ</a>
+                                </li>
+                               <li class="nav-item">
+                               <a class="nav-link" href="session.php"><i class="fa fa-cart-plus" aria-hidden="true" >&nbsp;</i>Giỏ Hàng</a>
+                               </li>
+                               <li class="nav-link">
+                                <form class="form-inline my-2 my-lg-0" style="margin-left:30px;" action="seach.php" method="POST">
+                                 <input class="form-control mr-sm-2" type="search" placeholder="Tìm kiếm" aria-label="Search" name="timkiem">
+                                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                                         <i class="fa fa-search" aria-hidden="true"></i>
+
+                                      </button>
+                              </form>
+                              </li>
                         
-                         
-                    </div>
-                </nav>
+                            </ul>
+                            
+                             
+                        </div>
+                    </nav>
+                </div>
             </div>
-        </div>
     </header>
     <div class="banner">
         <div class="autoplay css_slider">
@@ -249,8 +261,7 @@ function VndDot($strNum)
 document.write('<style type="text/css">body{background:#f5465a;padding-bottom:150px}</style><img style="position:fixed;z-index:9999;top:0;left:0" src="https://lh6.googleusercontent.com/-c8CoUvCourw/UMcWWTeBE-I/AAAAAAAABcU/q-j9X7733zw/s150/top-left.png"/><img style="position:fixed;z-index:9999;top:0;right:0" src="https://lh5.googleusercontent.com/-JABklf9ModU/UMcWWzSSJ4I/AAAAAAAABcY/g4sZMhrgjXU/s150/top-right.png"/><div style="position:fixed;z-index:9999;bottom:0;left:0;width:100%;height:104px;background:url(https://smartbb.googlecode.com/svn/trunk/dgm-footer.png) repeat-x bottom left;"></div><img style="position:fixed;z-index:9999;bottom:30px;left:30px" src="https://lh4.googleusercontent.com/-mEgGUg-ou4k/UMcz0qy2NhI/AAAAAAAABc0/gF1uW4iE6y0/s180/bottom-left.png"/>');var no=100;var hidesnowtime=0;var snowdistance='pageheight';var ie4up=(document.all)?1:0;var ns6up=(document.getElementById&&!document.all)?1:0;function iecompattest(){return(document.compatMode&&document.compatMode!='BackCompat')?document.documentElement:document.body}var dx,xp,yp;var am,stx,sty;var i,doc_width=800,doc_height=600;if(ns6up){doc_width=self.innerWidth;doc_height=self.innerHeight}else if(ie4up){doc_width=iecompattest().clientWidth;doc_height=iecompattest().clientHeight}dx=new Array();xp=new Array();yp=new Array();am=new Array();stx=new Array();sty=new Array();for(i=0;i<no;++i){dx[i]=0;xp[i]=Math.random()*(doc_width-50);yp[i]=Math.random()*doc_height;am[i]=Math.random()*20;stx[i]=0.02+Math.random()/10; sty[i]=0.7+Math.random();if(ie4up||ns6up){document.write('<div id="dot'+i+'" style="POSITION:absolute;Z-INDEX:'+i+';VISIBILITY:visible;TOP:15px;LEFT:15px;"><span style="font-size:18px;color:#fff">*</span></div>')}}function snowIE_NS6(){doc_width=ns6up?window.innerWidth-10:iecompattest().clientWidth-10;doc_height=(window.innerHeight&&snowdistance=='windowheight')?window.innerHeight:(ie4up&&snowdistance=='windowheight')?iecompattest().clientHeight:(ie4up&&!window.opera&&snowdistance=='pageheight')?iecompattest().scrollHeight:iecompattest().offsetHeight;for(i=0;i<no;++i){yp[i]+=sty[i];if(yp[i]>doc_height-50){xp[i]=Math.random()*(doc_width-am[i]-30);yp[i]=0;stx[i]=0.02+Math.random()/10;sty[i]=0.7+Math.random()}dx[i]+=stx[i];document.getElementById('dot'+i).style.top=yp[i]+'px';document.getElementById('dot'+i).style.left=xp[i]+am[i]*Math.sin(dx[i])+'px'}snowtimer=setTimeout('snowIE_NS6()',10)}function hidesnow(){if(window.snowtimer){clearTimeout(snowtimer)}for(i=0;i<no;i++)document.getElementById('dot'+i).style.visibility='hidden'}if(ie4up||ns6up){snowIE_NS6();if(hidesnowtime>0)setTimeout('hidesnow()',hidesnowtime*1000)}
 //]]>
 </script>
-<script type='text/javascript' src='http://hoangluyen.com/demo/js/snow.mini.js'></script>
-<script type="text/javascript" src="https://itexpress.vn/js/noel.js"></script>
+
 
     <!-- <script type="text/javascript" src="http://demo.iwebs.vn/api/js/noel.js"></script> -->
     <!-- <script type="text/javascript" src="js/noel.js"></script> -->
