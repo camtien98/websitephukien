3b98f3aae6bf0aa437487b8bc1c53a7225dbd9f8
camtien98 <57025344+camtien98@users.noreply.github.com>
2019-12-21 03:31:09 +0700

    a

---
+++ subpage/khongtimkiem.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TTShop</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="slick/slick.css">
+
+    <link rel="stylesheet" href="font/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=K2D:400,400i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playball" rel="stylesheet">
+    
+    <link rel="stylesheet" type="text/css" href="css/set1.css" />
+    <style>/*set a border on the images to prevent shifting*/
+        #gallery_01 img{border:2px solid white;}
+        
+        /*Change the colour*/
+        .active img{border:2px solid #333 !important;}</style>
+        <div id="christmas-tree-snowman-animated"></div>
+        
+</head>
+
+<body>
+    <div class="body">
+        <!-- body -->
+
+        <header class="nav-top head">
+            <div class="container">
+                <div class="row">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="#">
+                            <!-- <img src="img/logo.png" class="logo" alt=""> -->
+                            Tiên Tiên
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse nav-right" id="navbarSupportedContent">
+
+                            <ul class="navbar-nav mr-auto ">
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="Trangchu.php">Trang Chủ </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="GioiThieusp.php">Giới Thiệu</a>
+                                </li>
+    
+                                <li class="nav-item">
+                                    <a class="nav-link" href="SanPham.php">Sản Phẩm</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="Lienhe.php">Liên Hệ</a>
+                                </li>
+                               <li class="nav-item">
+                               <a class="nav-link" href="session.php"><i class="fa fa-cart-plus" aria-hidden="true" >&nbsp;</i>Giỏ Hàng</a>
+                               </li>
+                               
+                            </ul>
+                            
+                             
+                        </div>
+                    </nav>
+                </div>
+                 
+
+                        
+            </div>
+
+        </header>
+        <div class="banner">
+            <div class="autoplay css_slider">              
+                <img src="img/2.jpg" alt="">
+              
+            </div>
+        </div>
+        
